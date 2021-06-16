1722e287b0690d0a9fbdc052f2e42f47b3d8986a
jeffersonSantos13 <yuiti.santos@gmail.com>
2021-06-15 21:48:20 -0300

    Adicionando miragejs, e Dashboard, header, summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
   );
 }
